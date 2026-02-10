a13aa7777bd5678932039f7c2076dd0b3c86f283
Nick <nikitareznov@gmail.com>
2026-02-10 19:57:56 +0200

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { ProductCard, CategoryCard, Slider } from '@/components/shared';
 import HomeContent from '@/components/shared/home/home-content';
-import { categories, products } from '@/data';
-import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Home() {
-  const images = ['/iphone.webp', '/iphone.webp', '/xiamo.webp'];
-
   return <HomeContent />;
 }
